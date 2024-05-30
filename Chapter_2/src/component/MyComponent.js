80ceb2261b350bc86d676e9d7f8fee0df0e83ec9
trinhuyenlht7 <trinhuyenlht7@email.com>
2024-05-30 20:46:52 +0700

    chapter_2 update

---
+++ Chapter_2/src/component/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class MyComponent extends React.Component{
+    render(){
+        return(
+            <div>My First HTML
+                {Math.random()}
+            </div>
+        )
+    }
+}
+
+export default MyComponent
