b50bf76708b784d66ddb03721b1af931eb21808f
trinhuyenlht7 <trinhuyenlht7@email.com>
2024-05-30 16:31:49 +0700

    first commit

---
+++ Chapter_1/bai1.js
@@ -0,0 +1,103 @@
+
+
+// class Novel {
+//     constructor(Veriable, Title, Author){
+//         this.Veriable = Veriable
+//         this.Title = Title
+//         this.Author = Author
+//     }
+// }
+
+// const myNovel = new Novel ('Value', 'Tôi thấy hoa vàng trên cỏ xanh', 'Nguyễn Ngọc Ánh')
+// console.log(myNovel)
+
+// let person = {
+//     name : 'Trịnh Uyên', age : 22
+// }
+// console.log(person.name)
+// console.log(person['name'])
+
+// test = (name) => console.log(`${name} + 1`)
+// test("Trịnh Uyên")
+// test2 = name => console.log(`${name} yêu Vy chó`)
+// test2('Uyên')
+
+// const test3 = () => {
+//     console .log('xin chào bạn')
+// }
+
+// console.log(test3)
+
+// const myArrs = ['apple','banana','orange']
+// const numbers = [1,2,3,4,5]
+
+// numbers.map((number,index)=>console.log(index, number))
+// const fruilts = myArr.map((fruild)=> `<p>${fruild}</p>`)
+
+// document.write(fruilts)
+
+// const a = numbers.filter((number)=> number >= 3)
+
+// console.log(a)
+
+// document.write(`hello my name is Uyên
+// I am 25 year old
+// I am gudboiz`)
+
+// // document.write('hello my name is Uyên I am 25 year old I am gudboiz')
+
+// const basr_url = 'localhost:8080'
+// const api = 'get-user' ; fetch_page = 2
+
+
+// console.log(`// ${basr_url}/${api}?page=${fetch_page}`)
+
+
+// const firstArr = [1,2,3,4,5]
+// const secondArr = [6,7,8]
+
+// const thirdArr = [...secondArr,...firstArr]
+
+// var thirdArrs = {...thirdArr} 
+
+// console.log(thirdArr) 
+// console.log(thirdArrs)
+
+let userAdmin = {
+    admin(){
+        console.log(`I am Uyên`)
+    }
+}
+
+let userGuest = {}
+// userGuest.userAdmin.admin()
+userGuest.admin?.()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
